594d68d4ddb4b53f43f15ef3f5dccf82be30a25b
VladRaileanu98 <alexandru98raileanu@yahoo.com>
2023-06-19 16:46:49 +0300

    ceva de backend

--- src/main/java/com/example/backend/security/jwt/JwtUtils.java
+++ src/main/java/com/example/backend/security/jwt/JwtUtils.java
@@ -22,9 +22,7 @@ public class JwtUtils {
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
-
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
-
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
                 .setIssuedAt(new Date())
