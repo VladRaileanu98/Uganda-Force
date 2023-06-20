f9a2b0e84ce3ce5045c8976277253169958337e2
VladRaileanu98 <alexandru98raileanu@yahoo.com>
2023-06-20 19:06:04 +0300

    change

--- src/main/java/com/example/backend/controller/QuizController.java
+++ src/main/java/com/example/backend/controller/QuizController.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("quiz")
 @RequestMapping("/quiz")
 public class QuizController {
     @Autowired
